ded8ac42289fd5d0aa2b10f0672087b9dc1d7b68
Ciprian Dragomir <ciprian@Ciprians-MacBook-Pro-2.local>
2016-01-29 16:15:03 +0000

    Refactored old code and organised the functionality into methods of the FontRemap js class.

--- font-remap.js
+++ font-remap.js
@@ -22,8 +22,8 @@ var argv = yargs
     .help('help')
     .argv;
 
+const FontRemap = require('./lib/FontRemap');
 if(argv['r']) {
-    const FontRemap = require('./lib/FontRemap');
     if(argv._.length < 1) {
         console.log('Please specify a valid font file to re-map.');
         return 1;
@@ -36,17 +36,20 @@ if(argv['r']) {
     FontRemap(argv._[0]).remap({
         src: argv._[0],
         dest: dest,
-        map: argv._[0] + '.font-map.js',
+        map: argv._[0] + '.font-map.json',
         ttx: __dirname + '/fonttools/lib/fontTools/ttx.py'
     });
 } else {
-    const str = argv['s'] || argv._[0];
+    const str = argv['s'] || argv._[0],
+        fontMap = argv['m'];
     if(_.isString(str)) {
-        console.log('String ', str);
         if(!_.isString(argv['m'])) {
             console.log('Please specify the font-mapping to use as a cypher for this string using option -m (--map).');
             return 1;
         }
+        FontRemap().encode(str, fontMap, (encodedString) => {
+            console.log(encodedString);
+        });
     } else {
         yargs.showHelp();
     }
