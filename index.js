e0b9aefc913858fbeb9541a5b5f2fff9412018ca
ciprian <ciprian@teluric.uk>
2016-01-31 13:58:50 +0000

    index.js intentions info.

--- index.js
+++ index.js
@@ -1,23 +1,6 @@
-//const child_process = require('child_process');
-//const exec = child_process.exec;
-//
-////var proc = child_process.spawn('python3',  [__dirname + '/fonttools/lib/fontTools/ttx.py' .ttx, '-f', __dirname + '/demo/RobotoCondensed-Regular.modified.ttf'], { encoding: 'utf8' });
-////proc = child_process.spawn('ps', { encoding: 'utf8' });
-////proc.stdout.on('data', (data) => { console.log(data); });
-////proc.on('close', (code, data) => {
-////    console.log(code);
-////    console.log(data);
-////});
-//const cmd = 'python3 ' + __dirname + '/fonttools/lib/fontTools/ttx.py' + ' -f ' + __dirname + '/demo/RobotoCondensed-Regular.modified.ttf';
-//console.log(cmd);
-//exec('python3 ' + __dirname + '/fonttools/lib/fontTools/ttx.py'+ '-f' + __dirname + '/demo/RobotoCondensed-Regular.modified.ttf',
-//    (error, stdout, stderr) => {
-//        if(error) {
-//            console.log(stderr);
-//        } else {
-//            console.log('stdout: ', stdout);
-//        }
-//    });
 
+
+// THIS WILL BE AMENDED WHEN THE PROJECT IS PUBLISHED TO NPM
+// such that it can be used by inclusion: require('font-remap');
 module.exports = require('./lib/FontRemap');
 
